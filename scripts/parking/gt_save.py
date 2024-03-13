b549cfd9c5973f5399e08eed583f523946340ddb
Sadanand Modak <sadanandm@utexas.edu>
2024-03-13 04:36:32 -0500

    changes

--- scripts/parking/gt_save.py
+++ scripts/parking/gt_save.py
@@ -19,3 +19,19 @@ if __name__ == "__main__":
         label_np = np.array(label_img).reshape((H, W))
         flat_label_np = label_np.reshape(-1).astype(np.uint8)
         flat_label_np.tofile(os.path.join(rootdir, f"{noext_img_name}.bin"))
+
+    hfdi = "sam1120/parking-terrain_marks"
+    rootdir = f"{nspl_root_dir}/evals_data_parking/utcustom/gt_terrainmarks"
+    os.makedirs(rootdir, exist_ok=True)
+    ds_dict = load_dataset(hfdi)
+    ds = ds_dict['train']
+    for i in range(len(ds)):
+        print(f"Processing {i+1}/{len(ds)}")
+        pil_img = ds[i]['pixel_values']
+        W, H = pil_img.size
+        label_img = ds[i]['labels']
+        img_name = ds[i]['name']
+        noext_img_name = os.path.splitext(img_name)[0]
+        label_np = np.array(label_img).reshape((H, W))
+        flat_label_np = label_np.reshape(-1).astype(np.uint8)
+        flat_label_np.tofile(os.path.join(rootdir, f"{noext_img_name}.bin"))
